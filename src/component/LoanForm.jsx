d63e1a1f7f1761d0eb397c5496a840984b684238
Harikrishnan Mohan <harikrishnanmohan@Harikrishnans-MacBook-Air.local>
2025-03-04 10:12:14 +0530

    fix: color change

--- src/component/LoanForm.jsx
+++ src/component/LoanForm.jsx
@@ -20,15 +20,20 @@ const LoanForm = ({ handleCalculate, handleReset }) => {
       <input type="number" placeholder="Tenure" name="tenure" required />
       <input
         type="number"
-        placeholder="Extra Principle"
+        placeholder="Extra Principle (optional)"
         name="extraPrinciple"
       />
-      <input type="number" placeholder="From Month" name="fromMonth" min={7} />
+      <input
+        type="number"
+        placeholder="From Month (if entering extra principle)"
+        name="fromMonth"
+        min={7}
+      />
       <div className="button_section">
-        <button type="reset" className="button" onClick={onReset}>
+        <button type="reset" className="button reset" onClick={onReset}>
           Reset
         </button>
-        <button type="submit" className="button">
+        <button type="submit" className="button success">
           Calculate
         </button>
       </div>
